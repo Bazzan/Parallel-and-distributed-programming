4653241499f4d296287e434d06ccb56a1869e0cd
Bazzan <sebake01@gmail.com>
2020-03-04 19:03:20 +0100

    Task1 -> map, filter, split upload

--- Erlang/ErlangVecka1/task1.erl
+++ Erlang/ErlangVecka1/task1.erl
@@ -2,7 +2,7 @@
 
 % -compile(export_all).
 
--export([eval/1, eval/2, ifTuple/1]).
+-export([eval/1, eval/2, ifTuple/1, map/2, filter/2, split/2]).
 
 eval(X) ->
   case X of
@@ -19,10 +19,10 @@ eval(X) ->
 eval(X,M) ->
   case X of  
     {Atom, A, B} when is_tuple(A) ->
-        eval({Atom,ifTuple(A), B});
+      eval({Atom,ifTuple(A), B}, M);
 
     {Atom, A, B} when is_tuple(B) ->
-    eval({Atom, A, ifTuple(B)});
+      eval({Atom, A, ifTuple(B)}, M);
 
     {Atom, A, B} when is_atom(A) ,is_map_key(A,M) ->
       eval ({Atom, maps:get(A,M), B}, M);
@@ -47,8 +47,42 @@ eval(X,M) ->
 
 ifTuple(X2) ->
     case X2 of
-      {add, A, B} -> A + B;
-      {sub, A, B} -> A - B;
-      {'div', A, B} -> A / B;
-      {mul, A, B} -> A * B
+    {Atom, A, B} when is_tuple(A) ->
+      ifTuple({Atom, ifTuple(A), B});
+    {Atom, A, B} when is_tuple(B) ->
+      ifTuple({Atom, A, ifTuple(B)});
+
+    {add, A, B} -> A + B;
+    {sub, A, B} -> A - B;
+    {'div', A, B} -> A / B;
+    {mul, A, B} -> A * B
+  end.
+
+
+map(F,L) -> map(F, L,[]).
+map(_,[], Acc) -> lists:reverse(Acc);
+map(F,[H|T], Acc) -> map(F,T,[F(H)|Acc]).
+
+
+filter(F,L) -> filter(F,L,[]).
+filter(_,[],Acc) -> lists:reverse(Acc);
+filter(F,[H|T],Acc) ->
+  case F(H) of
+    true -> 
+      filter(F,T,[H|Acc]);
+    false -> 
+      filter(F,T,Acc)
     end.
+
+split(F,L) -> split(F,L,[],[]).
+split(_,[],True,False) -> 
+  {lists:reverse(True), lists:reverse(False)};
+split(F,[H|T],True,False) -> 
+  case F(H) of
+    true ->
+      split(F,T,[H|True],False);
+    false ->
+      split(F,T,True,[H|False])
+    end.
+
+% lists:reverse(True), lists:reverse(False), {True,False};
